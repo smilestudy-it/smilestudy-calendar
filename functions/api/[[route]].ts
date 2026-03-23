d9a5b9f00f8b9ce1db12fa4b61b70f0af6da0f54
hiromi1630 <bigsea1630@docomo.ne.jp>
2026-03-23 12:41:19 +0900

    feat: introduce auth0 to hono

--- functions/api/[[route]].ts
+++ functions/api/[[route]].ts
@@ -1,10 +1,32 @@
 import { Hono } from 'hono';
+import { jwk } from 'hono/jwk';
 import { handle } from 'hono/cloudflare-pages';
+import type { Context, Next } from 'hono';
+import type { JwtVariables } from 'hono/jwt';
 
-const app = new Hono({ strict: false }).basePath('/api');
+type Bindings = Env & {
+  AUTH0_AUDIENCE: string;
+  AUTH0_ISSUER: string;
+  AUTH0_JWKS_URI: string;
+};
 
-app.get('/', (c) => {
-  return c.json({ message: 'Hello' });
+const app = new Hono<{ Bindings: Bindings; Variables: JwtVariables }>().basePath('/api');
+
+const auth = async (c: Context<{ Bindings: Bindings; Variables: JwtVariables }>, next: Next) =>
+  jwk({
+    jwks_uri: c.env.AUTH0_JWKS_URI,
+    alg: ['RS256'],
+    verification: {
+      aud: c.env.AUTH0_AUDIENCE,
+      iss: c.env.AUTH0_ISSUER,
+    },
+  })(c, next);
+
+app.get('/', (c) => c.json({ message: 'Hello' }));
+
+app.get('/protect', auth, (c) => {
+  const { sub } = c.var.jwtPayload; 
+  return c.json({ message: 'auth success', userId: sub });
 });
 
 export const onRequest = handle(app);
--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,8 @@ createRoot(document.getElementById('root')!).render(
       domain={import.meta.env.VITE_AUTH0_DOMAIN}
       clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
       authorizationParams={{
-        redirect_uri: window.location.origin
+        redirect_uri: window.location.origin,
+        audience: import.meta.env.VITE_AUTH0_AUDIENCE
       }}
     >
     <App />
